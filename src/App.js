3f495760873fd3743feb6ade441bb0fe4597d3e7
hmuratoski <98337481+hmuratoski@users.noreply.github.com>
2022-09-16 16:27:04 +0300

    equation & conditionals;

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ const[bottles,setBottles]=useState(0);
 const[gender,setGender]=useState("male");
 const[time,setTime]=useState(0);
 const[alcoholLevel,setAlcoholLevel]=useState(0);
+//calculation equation & conditionals;
+const calculate=(e)=>{
+  let litres=bottles * 0.33;
+  let grams= litres * 8 * 4.5;
+  let burning = weight/10;
+  let gramsL = grams - (burning * time);
 
+  let results = 0;
+  if (gender=== "male") {
+    result = gramsL / (weight * 0.7)
+  }
+  else{
+    result = gramsL / (weight * 0.6)
+  }
+  if (result<0) {result = 0
+    setAlcoholLevel(result.toFixed(2))
+  }
+}
   return (
    
   );
